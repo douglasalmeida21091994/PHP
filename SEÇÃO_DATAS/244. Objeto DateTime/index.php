524e0c153d36b04af459d3d2b1f6edbeb22420f7
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2023-10-23 05:59:54 -0300

    PHP

---
+++ SEÇÃO_DATAS/244. Objeto DateTime/index.php
@@ -0,0 +1,12 @@
+<?php
+
+# O objeto DateTime() permite tratar a data como um objeto;
+# Podemos passar um parâmetro que será a data criada, se não passarmos nada a data será a atual.
+# Podemos exibir as informações do objeto com print_r
+
+# Exemplo:
+
+# $dataAtual = new DateTime();
+
+$dataAtual = new DateTime();
+print_r($dataAtual);
