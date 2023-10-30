be62ff575674658bf64945a6b99945f2d8a7007c
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2023-10-29 22:04:37 -0300

    252. Introdução ao HTTP

---
+++ SEÇÃO_14_PHP e a WEB/252. Introdução ao HTTP/index.php
@@ -0,0 +1,12 @@
+<?php
+
+# A web roda em cima do protocolo HTTP (HyperText Transfer Protocol);
+
+# Quando um navegador solicita uma página web é feito um request HTTP;
+
+# Esta requisição recebe uma resposta, ambos podem possuir um body;
+
+# A resposta contém um header, que é construído pelo método (GET/POST), arquivo/path solicitado (index.php) e versão do protocolo HTTP (HTTP/1.x);
+
+# Basicamente uma requisição é enviada e uma resposta é recebida;
+
