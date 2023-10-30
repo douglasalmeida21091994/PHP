72970e9e26810bc9646317c84fb14647e19c40bb
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2023-10-29 22:18:43 -0300

    254. Variáveis globais do PHP

---
+++ SEÇÃO_14_PHP e a WEB/254. Variáveis globais do PHP/index.php
@@ -0,0 +1,13 @@
+<?php
+
+# Para lider com estas requisições o PHP nos dá algumas variáveis globais:
+
+# $_ENV > variáveis de ambiente;
+
+# $_GET > parâmetros que foram enviados por request GET
+# $_POST > Parâmetros que foram enviados por POST
+
+# $_COOKIE > valores de cookies
+# $_SERVER > Informações do servidor
+
+# $_FILES > Informações sobre os arquivos que vieram por upload
