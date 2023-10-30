8aa03dacc69e196eb94b9d30e87b1793d5419eec
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2023-10-29 22:12:59 -0300

    253. Métodos HTTP

---
+++ SEÇÃO_14_PHP e a WEB/253. Métodos HTTP/index.php
@@ -0,0 +1,14 @@
+<?php
+
+# As requisições que enviamos também contém métodos, alguns deles são:
+
+#GET > Solicita a apresentação de um recurso ( Ex.: visualização de uma página);
+
+# POST > Envio de dados ao servidor (Ex.: cadastro de usuário);
+
+# PUT > Atualização completa de dados
+
+# DELETE > Remoção de dados;
+
+# PATCH > Atualização de dado específico;
+
