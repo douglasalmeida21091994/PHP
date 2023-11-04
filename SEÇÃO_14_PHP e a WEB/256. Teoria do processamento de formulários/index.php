15314ea8462e8d281f5b9e0e52ab9d2e32279a94
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2023-11-04 16:07:15 -0300

    256. Teoria do processamento de formulários

---
+++ SEÇÃO_14_PHP e a WEB/256. Teoria do processamento de formulários/index.php
@@ -0,0 +1,10 @@
+<?php
+
+# Vamos realizar processamentos de formulários de duas maneiras: GET e POST
+
+# GET -> vamos processar os parâmetros que vem na query string, ou seja, na URL
+
+# POST -> vamos processar as informações que vem na requisição, estas não aparecem na URL
+
+#Exemplo: BUSCAS => GET | REGISTRO DE USUÁRIO => POST
+
